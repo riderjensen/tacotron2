511d07fffda4a439a2325495cc4ecc1f3293b1be
PC@home <weiluomiic@gmail.com>
2020-07-29 17:48:24 +0200

    Added IPA preprocessing with hparam

--- data_utils.py
+++ data_utils.py
@@ -4,7 +4,7 @@ import torch
 import torch.utils.data
 
 import layers
-from utils import load_wav_to_torch, load_filepaths_and_text
+from utils import load_wav_to_torch, load_filepaths_and_text, convert_to_ipa
 from text import text_to_sequence
 
 
@@ -16,6 +16,8 @@ class TextMelLoader(torch.utils.data.Dataset):
     """
     def __init__(self, audiopaths_and_text, hparams):
         self.audiopaths_and_text = load_filepaths_and_text(audiopaths_and_text)
+        if hparams.ipa_preprocessing:
+            convert_to_ipa(self.audiopaths_and_text)
         self.text_cleaners = hparams.text_cleaners
         self.max_wav_value = hparams.max_wav_value
         self.sampling_rate = hparams.sampling_rate
